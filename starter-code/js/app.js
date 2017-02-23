a4ba731ff514e267fd5812caac40bb4066f36dca
Thelma Boamah <thelma.a.boamah@gmail.com>
2017-02-22 23:40:08 -0800

    homework done

--- starter-code/js/app.js
+++ starter-code/js/app.js
@@ -1,5 +1,20 @@
 console.log('app.js loaded!');
 
 // initialize the application
-angular
+var app = angular
   .module("hangmanApp", []);
+
+app.controller("HangmanController", HangmanController);
+
+function HangmanController(){
+
+	var vm = this;
+
+	vm.game = new HangmanGame('mercurial');
+
+	vm.guessLetter = function(input) {
+    vm.game.guess(input);
+    vm.game.input = "";
+  }
+
+}
