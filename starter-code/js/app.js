92194493c14d0c6ea536d55140585ff7b3da97da
monzelb <monzelb@gmail.com>
2017-02-23 09:09:15 -0800

    copied work over from a branch that wouldnt commit from some reason

--- starter-code/js/app.js
+++ starter-code/js/app.js
@@ -1,5 +1,21 @@
 console.log('app.js loaded!');
 
-// initialize the application
+var app = angular.module("hangmanApp", []);
+app.controller("HangmanController", HangmanController)
+// App
 angular
-  .module("hangmanApp", []);
+  .module("hangmanApp", [])
+  .controller("HangmanController", HangmanController);
+
+function HangmanController() {
+  var vm = this;
+  // create a new game
+  vm.hangman = new HangmanGame("elephant");
+  
+  vm.guessLetter = function(input) {
+    // guess letter
+    vm.hangman.guess(input);
+    // clear input
+    vm.hangman.input = "";
+  }
+}
