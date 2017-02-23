5fb54a7536de72ff7c5fdc6a88b900f206d3a445
zeroCalm <smith.timothyh@gmail.com>
2017-02-22 19:04:10 -0800

    the game shows the letters guessed as they are pressed

--- starter-code/js/app.js
+++ starter-code/js/app.js
@@ -1,5 +1,13 @@
 console.log('app.js loaded!');
 
 // initialize the application
-angular
-  .module("hangmanApp", []);
+var app = angular.module("hangmanApp", []);
+app.controller("hangmanCtrl", hangmanCtrl)
+
+
+function hangmanCtrl(){
+  var vm = this;
+   vm.game = new HangmanGame('elephant');
+
+   
+}
