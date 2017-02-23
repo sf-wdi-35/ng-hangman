7061ecb662f1570c6b57c7250badf0e46f6952d7
kkuo57 <kkuo507@gmail.com>
2017-02-23 09:06:40 -0800

    first commit that works

--- starter-code/js/app.js
+++ starter-code/js/app.js
@@ -2,4 +2,15 @@ console.log('app.js loaded!');
 
 // initialize the application
 angular
-  .module("hangmanApp", []);
+  .module("hangmanApp", [])
+  .controller("HangmanController", HangmanController);
+
+function HangmanController() {
+  var vm = this;
+
+  vm.hangman = new HangmanGame('bazooka', 5);
+  vm.guessLetter = function(letter){
+    vm.hangman.guess(letter);
+    vm.hangman.input = "";
+  }
+}
