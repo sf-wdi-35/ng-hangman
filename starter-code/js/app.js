1398820767b88e72057eb536c6e1daae197cf543
zeroCalm <smith.timothyh@gmail.com>
2017-02-22 22:02:33 -0800

    Almost through step three, did some of 4

--- starter-code/js/app.js
+++ starter-code/js/app.js
@@ -1,4 +1,4 @@
-console.log('app.js loaded!');
+//console.log('app.js loaded!');
 
 // initialize the application
 var app = angular.module("hangmanApp", []);
@@ -7,7 +7,18 @@ app.controller("hangmanCtrl", hangmanCtrl)
 
 function hangmanCtrl(){
   var vm = this;
-   vm.game = new HangmanGame('elephant');
+   vm.game = new HangmanGame('elephant', 8);  //Let's give em an extra shot.
 
-   
-}
+   vm.guessLetter = function(input){
+      console.log(input);  //attempt to get results from this function
+
+   }
+ }
+  //  vm.guessLetter = function(input) {
+  //   // guess the letter
+  //   console.log("I am guessing the letter");
+  // }
+  // //   vm.hangman.guess(input);
+  // //   // clear the input
+  // //   vm.hangman.input = "";
+  // // }
