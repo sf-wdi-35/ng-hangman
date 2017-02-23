f3e022decfd6f4117fbbc33943549c7e7fc65582
klawton1 <klawton1@gmail.com>
2017-02-23 10:30:29 -0800

    hangman

--- starter-code/js/app.js
+++ starter-code/js/app.js
@@ -1,5 +1,21 @@
 console.log('app.js loaded!');
 
 // initialize the application
-angular
-  .module("hangmanApp", []);
+var app = angular.module("hangmanApp", []);
+app.controller("hangmanController", hangmanController);
+function hangmanController(){
+  this.game = new HangmanGame(5);
+  this.guess = "";
+  this.guessLetter = function(){
+    this.game.guess(this.guess);
+    this.guess = "";
+    if(this.game.gameWon){
+      this.game.glyph = "glyphicon-ok";
+      this.game.reset();
+    }else if(!this.game.gameWon === false){
+      this.game.glyph = "glyphicon-remove";
+      debugger;
+      this.game.reset();
+    }
+  }
+}
